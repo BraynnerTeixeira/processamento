4bb0a7e573f2a63299526d28880bef61053e7981
Braynner Teixeira <42365689+BraynnerTeixeira@users.noreply.github.com>
2019-10-14 22:13:23 -0300

    Update rec_placa.py

--- rec_placa.py
+++ rec_placa.py
@@ -5,8 +5,10 @@ import tkinter
 import cv2
 
 
+
 #Carregando a imagem originl
 img = cv2.imread('D:\\Processamento de Imagens\\Reconhecimento de Placa\\imagens\\1.png')
+#img = img[::10,::10]
 
 
 #================================== Pré Processamento ==================================
@@ -25,8 +27,15 @@ lap = np.uint8(np.absolute(lap))
 
 #Filtro Canny
 canny1 = cv2.Canny(blur, 20, 120)
-cv2.imshow("Liminar MAis baixo", lap)
+#cv2.imshow("Liminar MAis baixo", lap)
 canny2 = cv2.Canny(blur, 70, 200)
-cv2.imshow("Liminar Mais Alto", lap)
+#cv2.imshow("Liminar Mais Alto", lap)
 
+#Aplicando o Filtro Sobel
+sobelX = cv2.Sobel(cinza, cv2.CV_64F, 1, 0) 
+sobelY = cv2.Sobel(cinza, cv2.CV_64F, 0, 1) 
+sobelX = np.uint8(np.absolute(sobelX)) 
+sobelY = np.uint8(np.absolute(sobelY)) 
+sobel = cv2.bitwise_or(sobelX, sobelY)
+cv2.imshow("Sobel", sobel)
 cv2.waitKey(0)
